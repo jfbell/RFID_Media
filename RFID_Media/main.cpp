a7d491b39a753cbca63f2cf30821842c1beed524
waspdesigned <james@JamesMBP.local>
2014-06-15 14:55:15 +1000

    Initial Commit

---
+++ RFID_Media/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RFID_Media
+//
+//  Created by James Bell on 15/06/2014.
+//  Copyright (c) 2014 WaspDesigned. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
